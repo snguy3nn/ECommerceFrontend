eeaadfc05eab547c5c81e45fd079446c97deb7cf
unknown <dylanbram354@gmail.com>
2021-07-16 19:36:13 -0400

    Removed comments

--- src/app.js
+++ src/app.js
@@ -7,12 +7,6 @@ import jwtDecode from 'jwt-decode';
 import NewListing from './components/NewListing/newListing';
 import NavBar from './components/NavBar/navBar';
 
-//NOTES Friday July 16
-//User login triggers redirect to LandingPage. LandingPage checks for token; renders appropriate page. 
-//BUT since NavBar is on App component, NavBar does NOT re-render on login. Requires refresh to pass new 'user' prop and render accordingly.
-//Likewise, logging OUT does NOT trigger re-render of LandingPage. Requires refresh after logout in order to show appropriate landing page.
-
-//**Just redirect all logged-out users to a login page with a link to register!!** Avoids conditional rendering for logged in/out
 
 export default function App(){
 
