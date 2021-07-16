5eb716955b826a9a0841c0e9d7b43ac4b1527037
unknown <dylanbram354@gmail.com>
2021-07-16 12:20:27 -0400

    Added and tested a few components with React Router

--- src/app.js
+++ src/app.js
@@ -1,12 +1,17 @@
 import React, { Component } from 'react';
 import { Switch, Route } from 'react-router-dom';
+import Login from './components/Login/login';
+import Register from './components/Register/register';
 
 class App extends Component {
     state = {  }
     render() { 
         return (
             <div>
-                
+                <Switch>
+                    <Route path="/register" component={Register} />
+                    <Route path="/login" component={Login} /> 
+                </Switch>
             </div>
          );
     }
---
+++ src/components/Register/register.jsx
@@ -0,0 +1,7 @@
+import React, { Component } from 'react';
+
+export default function Register(){
+    return(
+        <h1>Register page woooo</h1>
+    )
+}
