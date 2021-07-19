5e919848c03d135c131407372f2bea7adff42f60
ArthurD25 <arthuro.oneal@gmail.com>
2021-07-19 12:55:02 -0400

    created a comment box for review.

--- src/components/Reviews/reviews.jsx
+++ src/components/Reviews/reviews.jsx
@@ -1,10 +1,12 @@
+
 import react, {useState} from 'react';
+import { Button } from 'react-bootstrap';
 import {FaStar } from  'react-icons/fa';
 const StarRating = () => {
     const [rating, setRating] = useState(null);
     const [hover, setHover] = useState(null)
     return (
-     <div>
+     <div className="text-center">
          {[...Array(5)].map((star, i) =>{
              const ratingValue = i + 1;
              return (
@@ -14,8 +16,36 @@ const StarRating = () => {
              </label> 
              );
          })}
+         <div >
+         <textarea
+            placeholder="What is your feedback on the game?"
+            style={styles.textarea}>
+         </textarea>
+         </div>
+         <Button style={styles.button}>Submit</Button>
     </div>
          );
 };
+const styles = {
+    container:{
+        display: "flex",
+        flexdirection: "column",
+        alignItems: "center",
+    },
+    textarea: {
+        border: "1px solid #a9a9a9",
+        borderRadius: 5,
+        width: 300,
+        margin: "20px 0",
+        minHeight: 100,
+        padding: 10
+    },
+    button: {
+        border: "1px solid #a9a9a9",
+        borderRadius: 5,
+        width: 300,
+        padding: 10
+    }
+}
 
 export default StarRating
